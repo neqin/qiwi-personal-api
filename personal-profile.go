55bea15d93f14a5afda1d91fa427caee46ba369e
hazar <neqins@gmail.com>
2019-06-28 13:59:42 +0300

    init

---
+++ personal-profile.go
@@ -0,0 +1,148 @@
+package qiwi
+
+import (
+	"encoding/json"
+	"time"
+)
+
+/*
+PersonProfile
+{
+  "authInfo": {},
+  "contractInfo": {},
+  "userInfo": {}
+}
+*/
+type PersonProfile struct {
+	AuthInfo     PersonProfileAuthInfo     `json:"authInfo"`
+	ContractInfo PersonProfileContractInfo `json:"contractInfo"`
+	UserInfo     PersonProfileUserInfo     `json:"userInfo"`
+}
+
+/*
+PersonProfileAuthInfo
+{
+    "boundEmail": "m@ya.ru",
+    "ip": "81.210.201.22",
+    "lastLoginDate": "2017-07-27T06:51:06.099Z",
+    "mobilePinInfo": {},
+    "passInfo": {},
+    "personId": 79683851815,
+    "pinInfo": {
+      "pinUsed": true
+    },
+    "registrationDate": "2017-01-07T16:51:06.100Z"
+  }
+*/
+type PersonProfileAuthInfo struct {
+	BoundEmail    string                     `json:"boundEmail"`
+	IP            string                     `json:"ip"`
+	LastLoginDate time.Time                  `json:"lastLoginDate"`
+	MobilePinInfo PersonProfileMobilePinInfo `json:"mobilePinInfo"`
+	PassInfo      PersonProfilePassInfo      `json:"passInfo"`
+	PersonID      int                        `json:"personId"`
+	PinInfo       struct {
+		PinUsed bool `json:"pinUsed"`
+	} `json:"pinInfo"`
+	RegistrationDate time.Time `json:"registrationDate"`
+}
+
+/*
+PersonProfileMobilePinInfo
+{
+  "lastMobilePinChange": "2017-07-13T11:22:06.099Z",
+  "mobilePinUsed": true,
+  "nextMobilePinChange": "2017-11-27T06:51:06.099Z"
+}
+*/
+type PersonProfileMobilePinInfo struct {
+	LastMobilePinChange time.Time `json:"lastMobilePinChange"`
+	MobilePinUsed       bool      `json:"mobilePinUsed"`
+	NextMobilePinChange time.Time `json:"nextMobilePinChange"`
+}
+
+/*
+PersonProfilePassInfo
+{
+  "lastPassChange": "2017-07-21T09:25:06.099Z",
+  "nextPassChange": "2017-08-21T09:25:06.099Z",
+  "passwordUsed": true
+}
+*/
+type PersonProfilePassInfo struct {
+	LastPassChange time.Time `json:"lastPassChange"`
+	NextPassChange time.Time `json:"nextPassChange"`
+	PasswordUsed   bool      `json:"passwordUsed"`
+}
+
+/*
+PersonProfileContractInfo
+{
+    "blocked": false,
+    "contractId": 79683851815,
+    "creationDate": "2017-01-07T16:51:06.100Z",
+    "features": [
+      ...
+    ],
+    "identificationInfo": [
+      {
+        "bankAlias": "QIWI",
+        "identificationLevel": "SIMPLE"
+      }
+    ]
+
+*/
+type PersonProfileContractInfo struct {
+	Blocked            bool
+	ContractID         int           `json:"contractId"`
+	CreationDate       time.Time     `json:"creationDate"`
+	Features           []interface{} `json:"features"`
+	IdentificationInfo []struct {
+		BankAlias           string `json:"bankAlias"`
+		IdentificationLevel string `json:"identificationLevel"`
+	} `json:"identificationInfo"`
+}
+
+/*
+PersonProfileUserInfo
+{
+  "defaultPayCurrency": 643,
+  "defaultPaySource": 7,
+  "email": null,
+  "firstTxnId": 10807097143,
+  "language": "string",
+  "operator": "Beeline",
+  "phoneHash": "lgsco87234f0287",
+  "promoEnabled": null
+}
+*/
+type PersonProfileUserInfo struct {
+	DefaultPayCurrency Currency `json:"defaultPayCurrency"`
+	DefaultPaySource   int      `json:"defaultPaySource"`
+	Email              string   `json:"email"`
+	FirstTxnID         int64    `json:"firstTxnId"`
+	Language           string   `json:"language"`
+	Operator           string   `json:"operator"`
+	PhoneHash          string   `json:"phoneHash"`
+	PromoEnabled       string   `json:"promoEnabled"`
+}
+
+/*
+Person’s Profile
+
+https://edge.qiwi.com/person-profile/v1/profile/current?<parameters>
+*/
+func (qiwi *QiwiPersonalApi) GetPersonProfile(param string) (*PersonProfile, error) {
+	resp, err := qiwi.sendRequest(qiwi.apiKey, "GET", "/person-profile/v1/profile/current?"+param, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var res PersonProfile
+	err = json.Unmarshal(resp, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
---
+++ qiwi.go
@@ -0,0 +1,100 @@
+package qiwi
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+const (
+	MaxIdleConnections int    = 20000
+	RequestTimeout     int    = 25
+	ApiUrl             string = "https://edge.qiwi.com"
+)
+
+type QiwiPersonalApi struct {
+	httpClient *http.Client
+	apiKey     string
+}
+
+func NewQiwiPersonalApiWithHttpClient(apiKey string, client *http.Client) *QiwiPersonalApi {
+	qiwi := new(QiwiPersonalApi)
+	qiwi.httpClient = client
+	qiwi.apiKey = apiKey
+	return qiwi
+}
+
+func NewQiwiPersonalApi(apiKey string) *QiwiPersonalApi {
+	client := &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: MaxIdleConnections,
+		},
+		Timeout: time.Duration(RequestTimeout) * time.Second,
+	}
+
+	return NewQiwiPersonalApiWithHttpClient(apiKey, client)
+}
+
+func (qiwi *QiwiPersonalApi) newRequest(apiKey, method, spath string, data map[string]interface{}) (req *http.Request, err error) {
+	var path = ApiUrl + spath
+	var body io.Reader
+	if len(data) > 0 {
+		s, err := json.Marshal(data)
+		if err != nil {
+			return nil, err
+		}
+		body = bytes.NewBuffer(s)
+	}
+	req, err = http.NewRequest(method, path, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+
+	return req, err
+}
+
+func (qiwi *QiwiPersonalApi) sendRequest(apiKey, method, spath string, data map[string]interface{}) (body []byte, err error) {
+	req, err := qiwi.newRequest(apiKey, method, spath, data)
+	response, err := qiwi.httpClient.Do(req)
+	if err != nil && response == nil {
+		return nil, err
+	} else {
+		body, err := ioutil.ReadAll(response.Body)
+		//log.Println(string(body))
+		if err != nil {
+			if response.Body != nil {
+				response.Body.Close()
+			}
+			return nil, err
+		}
+		if response.StatusCode != 200 {
+			return nil, errors.New(response.Status)
+		}
+		if response.Body != nil {
+			response.Body.Close()
+		}
+		return body, nil
+	}
+}
+
+/*
+QIWI Currency
+*/
+type Currency int
+
+/*
+Currency codes (ISO 4217)
+*/
+const (
+	RUB Currency = 643
+	USD Currency = 840
+	EUR Currency = 978
+)
